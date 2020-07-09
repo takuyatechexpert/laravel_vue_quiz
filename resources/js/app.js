43c051bf61d369f204c0feaa472ba60d120c16e6
takuyatechexpert <lp4546.sg.abc@gmail.com>
2020-07-09 10:12:51 +0900

    add toppage vue-router

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
+import Vue from 'vue'
+import router from './router.js'
+import SocialSharing from 'vue-social-sharing'
+
 require('./bootstrap');
+
+Vue.use(SocialSharing);
+
+new Vue({
+  router: router, // routerにはrouter.jsファイルを設定
+ }).$mount('#app')
