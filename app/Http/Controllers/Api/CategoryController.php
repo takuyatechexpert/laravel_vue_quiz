f6843e1c50f6de5d52d88ee6bd36c591ad76bd6f
takuyatechexpert <lp4546.sg.abc@gmail.com>
2020-07-17 10:29:43 +0900

    add Category seeder

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -4,8 +4,16 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Category;
 
 class CategoryController extends Controller
 {
     //
+    public function index()
+  {
+    $category = Category::select('id', 'name')->get();
+
+    return $category;
+  }
+
 }
