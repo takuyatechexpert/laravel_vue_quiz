1c7269e07b6a601fc7e4e873297251f91f5f2422
takuyatechexpert <lp4546.sg.abc@gmail.com>
2020-08-01 17:42:51 +0900

    fix form tag

--- resources/js/components/module/TheModal.vue
+++ resources/js/components/module/TheModal.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <form action="/insertRanking" method="POST" id="finish-form">
     <div id="modal-result" tabindex="-1" class="modal fade" aria-hidden="true" role="dialog">
       <div class="modal-dialog">
         <div class="modal-content">
@@ -12,7 +12,8 @@
           <div class="modal-body text-center">
             <pie-chart :chartData="chartData" ref="chart"></pie-chart>
             <div>正解率 {{ correctPercentageObject['correctScore'] * 10 }} %</div>
-            <input type="hidden" name="correctRatio" />
+            <input type="hidden" name="correctRatio" :value="correctPercentageObject['correctScore'] * 10" />
+            <input type="hidden" name="_token" :value="csrf" />
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-primary" @click="quizFinish">終了する</button>
@@ -20,7 +21,7 @@
         </div>
       </div>
     </div>
-  </div>
+  </form>
 </template>
 
 <script>
@@ -40,12 +41,17 @@ export default {
       chartOptions: {
         hoverBorderWidth: 20
       },
+
       chartData: {
         hoverBackgroundColor: "red",
         hoverBorderWidth: 10,
         labels: ["正解", "不正解"],
         datasets: []
       },
+
+      csrf: document
+        .querySelector('meta[name="csrf-token"]')
+        .getAttribute("content")
     };
   },
   methods: {
@@ -62,7 +68,7 @@ export default {
       this.$refs.chart.renderPieChart();
     },
     quizFinish() {
-      location.href = "/";
+      document.querySelector("#finish-form").submit();
     }
   }
 };
