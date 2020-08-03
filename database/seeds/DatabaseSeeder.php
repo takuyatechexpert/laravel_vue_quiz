1feadf0733393da525307eaf890597c0347256b6
takuyatechexpert <lp4546.sg.abc@gmail.com>
2020-08-03 10:15:40 +0900

    fix seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+    $this->call(QuizTableSeeder::class);
+    $this->call(AnswerTableSeeder::class);
+    $this->call(CategoryTableSeeder::class);
+    $this->call(InformationTableSeeder::class);
+    $this->call(RankingTableDummySeeder::class);
     }
 }
