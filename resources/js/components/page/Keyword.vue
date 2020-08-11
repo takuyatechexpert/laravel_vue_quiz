c0ae1a716e06e69086a466cae57fa0ac5cad5392
takuyatechexpert <lp4546.sg.abc@gmail.com>
2020-08-11 10:19:23 +0900

    add keyword page and scss file

---
+++ resources/js/components/page/Keyword.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <main>
+      <div class="container">
+        <article class="col-md-8 col-xs-12">
+          <section>
+            <h2 class="keyword-h2">
+              <img class="keyword__logo" src="images/search-mark.png" />
+              【{{ }}】からはじまるキーワード（{{ }}語)
+            </h2>
+            <dl>
+              <dt>{{ }}</dt>
+              <dd class="keyword__description">
+                {{ }}
+                <p class="text-right">カテゴリー：{{ }}</p>
+              </dd>
+            </dl>
+          </section>
+        </article>
+        <the-sidebar></the-sidebar>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+import TheSidebar from "../layout/TheSidebar";
+
+export default {
+  components: {
+    TheSidebar,
+  },
+}
+</script>
